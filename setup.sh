5a2543374335ffb6d3c4c185c743d7b08f8dad8e
Vincent Lynch <vincent.lynch@shopify.com>
2021-12-12 22:12:16 -0500

    Test 12

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # jc --unit dotfiles.service -f
 # sc restart dotfiles.service
 # if using apt-get add "-o DPkg::Lock::Timeout=600"
@@ -7,7 +9,7 @@ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O 
 echo "Copying custom theme"
 cp ~/dotfiles/inspect.zsh-theme ~/.oh-my-zsh/themes
 echo "Modifying .zshrc plugins"
-sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="inspect"/g' ~/.zshrc
+sed -i 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"inspect\"/g' ~/.zshrc
 sed -i 's/plugins=(git/plugins=(git git-prompt/g' ~/.zshrc
 echo "Initializing ohmyzhs"
 source ~/.zshrc

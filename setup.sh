96f693b3ae3683b8cbf2163c5a218cc50ad91217
Vincent Lynch <vincent.lynch@shopify.com>
2021-12-12 22:27:26 -0500

    Final test Dec 12

--- setup.sh
+++ setup.sh
@@ -2,6 +2,10 @@
 # sc restart dotfiles.service
 # if using apt-get add "-o DPkg::Lock::Timeout=600"
 
+echo "Setting basic alias"
+alias ll="ls -al"
+alias c=clear
+
 echo "Installing ohmyzhs"
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 echo "Copying custom theme"
@@ -12,6 +16,4 @@ sed -i 's/plugins=(git/plugins=(git git-prompt/g' ~/.zshrc
 echo "Initializing ohmyzhs"
 source ~/.zshrc
 
-echo "Setting basic alias"
-alias ll="ls -al"
-alias c=clear
+

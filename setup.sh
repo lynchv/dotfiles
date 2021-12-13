6d46e9066b8731d2bbfcac81e467a16915050059
Vincent Lynch <vincent.lynch@shopify.com>
2021-12-12 21:54:01 -0500

    Test 6

--- setup.sh
+++ setup.sh
@@ -5,13 +5,11 @@
 echo "Installing ohmyzhs"
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 echo "Copying custom theme"
-pwd
 cp ~/dotfiles/inspect.zsh-theme ~/.oh-my-zsh/themes
 echo "Adding plugins"
-sed -i '' 's/plugins=(git/plugins=(git git-prompt/g' ./.zshr
+sed -i '' 's/plugins=(git/plugins=(git git-prompt/g' ~/.zshr
 echo "Initializing ohmyzhs"
-cd ~
-source ./.zshrc
+source ~/.zshrc
 
 echo "Setting basic alias"
 alias ll="ls -al"

e1cf3347e69033eb2cc9b1ecbce2f33d5d2b7af4
Vincent Lynch <vincent.lynch@shopify.com>
2021-12-12 21:39:51 -0500

    Test 3

--- setup.sh
+++ setup.sh
@@ -2,8 +2,13 @@
 # sc restart dotfiles.service
 # if using apt-get add "-o DPkg::Lock::Timeout=600"
 
-echo "Installing ohmyzhs..."
+echo "Installing ohmyzhs"
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+echo "Reading ohmyzhs config..."
+cd ~
+sed -i '' 's/plugins=(git/plugins=(git git-prompt/g' ./.zshr
+source ./.zshrc
 
-echo "Setting basic alias..."
+echo "Setting basic alias"
+alias ll="ls -al"
 alias c=clear

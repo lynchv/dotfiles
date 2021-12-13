e5664ac8ff7368622dcb06c1124cab81e85e1742
Vincent Lynch <vincent.lynch@shopify.com>
2021-12-12 20:26:18 -0500

    Trying something

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
-echo "Will spin print this?"
+echo "Installing ohmyzhs..."
+
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# if using apt-get add "-o DPkg::Lock::Timeout=600"
